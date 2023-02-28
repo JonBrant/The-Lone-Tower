fcfb4803706edf8d8dbcfe64fe2648ede16b79c6
JonBrant <jonbrant@gmail.com>
2023-02-28 18:49:40 -0500

    UpgradePanel started

---
+++ Assets/_IdleTowerDefense/Scripts/UI/UpgradePanel.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Michsky.UI.Shift;
+using UnityEngine;
+
+public class UpgradePanel : MonoBehaviour
+{
+    [SerializeField] private UpgradeButton upgradeButtonPrefab;
+    [SerializeField] private Transform buttonContainer;
+
+    [SerializeField] private AudioSource buttonAudioSource;
+
+    private void Start()
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            UpgradeButton button = Instantiate(upgradeButtonPrefab, buttonContainer);
+            button.GetComponent<UIElementSound>().audioObject = buttonAudioSource;
+        }
+    }
+}
