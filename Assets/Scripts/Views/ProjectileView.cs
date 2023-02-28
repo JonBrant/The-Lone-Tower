e3afa7976dbc81221839d22f0f9a3d8fc863e691
JonBrant <jonbrant@gmail.com>
2023-02-28 07:27:39 -0500

    ToDo

--- Assets/Scripts/Views/ProjectileView.cs
+++ Assets/Scripts/Views/ProjectileView.cs
@@ -36,7 +36,7 @@ public class ProjectileView : MonoBehaviour
         {
             if (enemyView.packedEntity.Unpack(world, out int unpackedEnemy) && packedEntity.Unpack(world, out int unpackedProjectile))
             {
-                // ToDo: Projectiles currently have no lifetime, separate destruction to allow for GameObject.Destroy(lifeTime)
+                // ToDo: Projectiles currently have no lifetime, separate destruction to allow for GameObject.Destroy(lifeTime) - Use HealthSystem, added before Destroy system
                 EcsPool<Destroy> destroyPool = world.GetPool<Destroy>();
                 EcsPool<Health> healthPool = world.GetPool<Health>();
                 EcsPool<Projectile> projectilePool = world.GetPool<Projectile>();
