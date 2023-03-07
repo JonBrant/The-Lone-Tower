101ad5360dc6b66d4580940771d4864d975380aa
JonBrant <jonbrant@gmail.com>
2023-03-07 00:56:52 -0500

    Health/Health regeneration upgrades

---
+++ Assets/_IdleTowerDefense/Scripts/Upgrades/HealthRegenerationUpgrade.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Health Regeneration Upgrade", menuName = "Idle Tower Defense/Upgrades/Health Regeneration")]
+public class HealthRegenerationUpgrade : UpgradeBase
+{
+    [Header("Upgrade Specific Values")]
+    public float HealthRegenerationPerUpgrade = 0.5f;
+    
+    private EcsFilter healthFilter;
+
+    public override void Init()
+    {
+        healthFilter = GameManager.Instance.World.Filter<Tower>().Inc<Health>().End();
+    }
+    
+    public override Dictionary<CurrencyTypes, float> GetCost()
+    {
+        return new Dictionary<CurrencyTypes, float> {
+            {
+                CurrencyTypes.Gold, UpgradeManager.Instance.UpgradeCounts[Title] + 1
+            }
+        };
+    }
+    
+    public override void Upgrade()
+    {
+        // Handle cost
+        GameManager.Instance.Currency.SubtractValues(GetCost());
+        UpgradeManager.Instance.UpgradeCounts[Title] += 1;
+
+        // Handle Upgrade
+        EcsPool<Health> healthPool = GameManager.Instance.World.GetPool<Health>();
+        foreach (int entity in healthFilter)
+        {
+            ref Health towerHealth = ref healthPool.Get(entity);
+            towerHealth.HealthRegeneration += HealthRegenerationPerUpgrade;
+        }
+    }
+}
