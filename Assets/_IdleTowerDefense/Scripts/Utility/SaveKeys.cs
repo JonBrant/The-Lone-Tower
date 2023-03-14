6feb6a4f927603584f3eba79e1fcac017de40f1b
JonBrant <jonbrant@gmail.com>
2023-03-14 02:46:43 -0400

    SaveKeys class to prevent usage of strings when saving

---
+++ Assets/_IdleTowerDefense/Scripts/Utility/SaveKeys.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveKeys
+{
+    public const string EnemiesKilled = "EnemiesKilled";
+    public const string Scrap = "Scrap;"
+}
